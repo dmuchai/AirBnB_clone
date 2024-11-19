c7f62889a5dc563623c535fa8b21394d05d77f80
Your Name <dmmuchai@gmail.com>
2024-11-19 04:29:54 -0800

    console

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -23,53 +23,49 @@ class FileStorage:
 
     def new(self, obj):
         """
-         Sets object in __objects dict with key <obj class name>.id.
+         Adds new object to __objects dict with key <obj class name>.id.
         """
-        obj_cls_name = obj.__class__.__name__
-
-        key = "{}.{}".format(obj_cls_name, obj.id)
-
-        FileStorage.__objects[key] = obj
-
+        if obj:
+            key = f"{obj.__class__.__name__}.{obj.id}"
+            self.__objects[key] = obj
 
     def all(self):
         """
         Returns the dictionary __objects.
         """
-        return  FileStorage.__objects
-
+        return self.__objects
 
     def save(self):
         """
         Serializes the __objects dict to the JSON format
         and saves it to the file specified by __file_path.
         """
-        all_objs = FileStorage.__objects
+        all_objs = self.__objects
 
-        obj_dict = {}
+        rep_dict = {}
 
         for obj in all_objs.keys():
-            obj_dict[obj] = all_objs[obj].to_dict()
+            rep_dict[obj] = all_objs[obj].to_dict()
 
-        with open(FileStorage.__file_path, "w", encoding="utf-8") as file:
-            json.dump(obj_dict, file)
+        with open(self.__file_path, "w", encoding="utf-8") as file:
+            json.dump(rep_dict, file)
 
     def reload(self):
         """
         Deserializes the JSON file to __objects dict,if it exists
         """
-        if os.path.isfile(FileStorage.__file_path):
-            with open(FileStorage.__file_path, "r", encoding="utf-8") as file:
+        if os.path.isfile(self.__file_path):
+            with open(self.__file_path, "r", encoding="utf-8") as file:
                 try:
-                    obj_dict = json.load(file)
+                    rep_dict = json.load(file)
 
-                    for key, value in obj_dict.items():
+                    for key, value in rep_dict.items():
                         class_name, obj_id = key.split('.')
 
                         cls = eval(class_name)
 
                         instance = cls(**value)
 
-                        FileStorage.__objects[key] = instance
+                        self.__objects[key] = instance
                 except Exception:
                     pass

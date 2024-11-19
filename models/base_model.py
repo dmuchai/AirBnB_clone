f268c5b1cf99735b90ad02050a232d1f1bed037d
Your Name <dmmuchai@gmail.com>
2024-11-19 07:39:44 -0800

    fixing errors

--- models/base_model.py
+++ models/base_model.py
@@ -33,7 +33,6 @@ class BaseModel:
     def save(self):
         """public instance attribute updated_at with the current datetime"""
         self.updated_at = datetime.utcnow()
-        models.storage.new(self) 
         models.storage.save()
 
     def to_dict(self):
